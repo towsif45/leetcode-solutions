fcb2642e02004f457ec11d8eb2294c228d77e84e
Towsif Hossain <towsifhossain5120@gmail.com>
2023-03-31 01:53:39 +0600

    121 added

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock .cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int mn = prices[0], res = 0;
+        for(int i=0; i<prices.size(); i++){
+            if(mn >= prices[i]) mn = prices[i];
+            else res = max(res, prices[i]-mn);
+        }
+        return res;
+    }
+};
